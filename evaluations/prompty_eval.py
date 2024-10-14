c7f9d2c35f25640ca46ae3dc72030babe59dc090
Paulo Lacerda <pclacerda@gmail.com>
2024-10-14 01:27:50 -0300

    featurex

--- evaluations/prompty_eval.py
+++ evaluations/prompty_eval.py
@@ -29,26 +29,26 @@ def main():
     print(details.head(10))
 
 
-    # Evaluation run
-    eval_prompty = "./evaluations/prompty-answer-score-eval.prompty"
-    eval_run = pf.run(
-        flow=eval_prompty,
-        data=data,  
-        run=base_run, 
-        column_mapping={
-            "question": "${data.question}",
-            "answer": "${run.outputs.output}",
-            "ground_truth": "${data.ground_truth}",
-        },
-        stream=True,
-    )
+    # # Evaluation run
+    # eval_prompty = "./evaluations/prompty-answer-score-eval.prompty"
+    # eval_run = pf.run(
+    #     flow=eval_prompty,
+    #     data=data,  
+    #     run=base_run, 
+    #     column_mapping={
+    #         "question": "${data.question}",
+    #         "answer": "${run.outputs.output}",
+    #         "ground_truth": "${data.ground_truth}",
+    #     },
+    #     stream=True,
+    # )
 
-    details = pf.get_details(eval_run)
+    # details = pf.get_details(eval_run)
 
-    print(details.head(10))
+    # print(details.head(10))
 
-    details = pf.get_details(eval_run)
-    details.to_excel("prompty-answer-score-eval.xlsx", index=False)
+    # details = pf.get_details(eval_run)
+    # details.to_excel("prompty-answer-score-eval.xlsx", index=False)
 
 
 if __name__ == '__main__':
